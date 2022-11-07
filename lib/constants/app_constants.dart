40fd65c557b3d23fb29205e88c50593dc2b7539c
avdavilag <avdavilag@utn.edu.ec>
2022-11-07 14:33:52 -0500

    Second Commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,7 @@
+abstract class ThingsboardAppConstants {
+  static final thingsBoardApiEndpoint = 'https://demo.thingsboard.io';
+  static final thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
+
+  /// Not for production (only for debugging)
+  static final thingsboardOAuth2AppSecret = 'Your app secret here';
+}
